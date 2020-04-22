01e93aba117ec94bc67bb4a81ad2ae5c8e275b07
Tim White <timothy.white@conga.com>
2020-04-22 12:35:27 -0600

    stubbed in gateway code

---
+++ services/Gateway/index.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'user', url: 'http://localhost:5000/graphql' },
+    // Define additional services here
+  ],
+});
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
