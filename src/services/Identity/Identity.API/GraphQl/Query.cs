9fab903b6ba3cebd510e50c7ec255005a89ef3ed
Tim White <timma.j.white@gmail.com>
2020-08-21 22:15:39 -0600

    fixed di for identity

--- src/services/Identity/Identity.API/GraphQl/Query.cs
+++ src/services/Identity/Identity.API/GraphQl/Query.cs
@@ -1,9 +1,17 @@
+using System;
 using Identity.API.Identity;
 
 namespace Identity.API.GraphQl
 {
     public class Query
     {
-        public IdentityType Identity() => new IdentityType();
+        private readonly IServiceProvider _serviceProvider;
+
+        public Query(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IdentityType Identity() => new IdentityType(_serviceProvider);
     }
 }
