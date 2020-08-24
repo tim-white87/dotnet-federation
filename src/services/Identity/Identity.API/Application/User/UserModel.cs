06883c7af9798e808a1182400032be5b8001ca16
Tim White <timothy.white@conga.com>
2020-08-24 17:17:33 -0600

    user model can die

--- src/services/Identity/Identity.API/Application/User/UserModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Identity.API.User
-{
-    public class UserModel
-    {
-        public string Id { get; set; }
-        public string Username { get; set; }
-        public string Name { get; set; }
-    }
-}
