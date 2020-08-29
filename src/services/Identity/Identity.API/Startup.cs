311623097f8691d1cbc252e0e01a177945e6f623
Tim White <timma.j.white@gmail.com>
2020-08-29 11:35:27 -0600

    removed old dead stuff and cleaned up structure

--- src/services/Identity/Identity.API/Startup.cs
+++ src/services/Identity/Identity.API/Startup.cs
@@ -27,7 +27,6 @@ namespace Identity.API
             Environment = environment;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMediatR(typeof(Startup));
@@ -45,7 +44,6 @@ namespace Identity.API
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
